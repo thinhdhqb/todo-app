0398451ea208773f1e40730a109453c6b6b6f095
Quang Thinh <thinhdhqb@gmail.com>
2022-09-12 23:17:35 +0700

    Create layout, allow input new tasks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function TodoListItem({ content }) {
+  return (
+    <Card className="my-1 shadow-lg rounded-none">
+      <CardContent className="align-middle">{content}</CardContent>
+    </Card>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function TodoList() {
+  const [todoList, setTodoList] = useState(["hello", "sdasa"]);
+	const inputRef = useRef('');
+
+	const handleInput = () => {
+		let newTask = inputRef.current.value;
+		if (!newTask) return;
+		setTodoList([...todoList, newTask]);
+		inputRef.current.value = '';
+		inputRef.current.focus();
+	}
+  return (
+    <>
+      <div className="flex">
+        <TextField
+          className="mr-3 w-full"
+          id="task-input"
+          label="New tasks"
+          color="primary"
+          size="medium"
+					inputRef={inputRef}
+					onKeyUp={e => {
+						if(e.key === 'Enter') handleInput();
+					}}
+        />
+        <Button className="bg-fuchsia-700 text-white inline" color="primary" onClick={handleInput}>
+          Add
+        </Button>
+      </div>
+      <div className="overflow-scroll max-h-full">
+        {todoList.map((item) => (
+          <TodoListItem content={item} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+const theme = createTheme({
+  status: {
+    danger: "#e53e3e",
+  },
+  palette: {
+    primary: {
+      main: "rgb(162 28 175)",
+      darker: "#053e85",
+    },
+    neutral: {
+      main: "#64748B",
+      contrastText: "#fff",
+    },
+  },
+});
+
+function App() {
+  return (
+    <div className="max-h-[80%] overflow-hidden w-4/5 rounded-md p-8 bg-white shadow-2xl flex-col">
+      <ThemeProvider theme={theme}>
+        <TodoList />
+      </ThemeProvider>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
